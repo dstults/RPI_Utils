0179ac80de9730eb031a897f0e8153a6ffb600b8
DranKof <drankof@gmail.com>
2021-12-19 17:48:47 -0800

    firmware upgrade should not occur, should prompt for os upgrade, used double quotes

--- TotalUpdate.sh
+++ TotalUpdate.sh
@@ -4,30 +4,23 @@ echo "Total Update Script"
 echo "==========================="
 read -p "Press [Enter] key to begin..."
 logger "Commencing 'TotalUpdate.sh' to update system"
-echo -n '[*] Refreshing repository cache...'
+echo -n "[*] Refreshing repository cache..."
 echo "==========================="
-sudo apt-get update -y
+sudo apt update -y
 echo "==========================="
-echo '[*] Repository cache refresh complete.'
-echo '[*] Performing full upgrade...'
+echo "[*] Repository cache refresh complete."
+echo "[*] Performing full upgrade..."
+echo "      (will require your confirmation if you want to do a full OS upgrade)"
 echo "==========================="
-sudo apt-get dist-upgrade -y
+sudo apt full-upgrade
 echo "==========================="
-echo '[*] Full upgrade complete.'
-echo '[*] Removing unused or cached packages...'
+echo "[*] Full upgrade complete."
+echo "[*] Removing unused or cached packages..."
 echo "==========================="
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
+sudo apt autoremove -y
+sudo apt autoclean -y
 echo "==========================="
-echo '[*] Package cleanup complete.'
-if [ $(which raspi-config | wc -l) -gt 0 ]; then
-        echo '[*] Raspberry Pi Detected.'
-        echo '[*] Update the Raspberry Pi firmware to the latest (if available)...'
-		echo "==========================="
-        sudo rpi-update
-		echo "==========================="
-        echo '[*] Done updating firmware.'
-fi
+echo "[*] Package cleanup complete."
 logger "Completing 'TotalUpdate.sh' system update"
 while true; do
         read -r -p "Do you wish to reboot? " choice
